eef9eb397145516d3d6da69992c72ce64b79ad37
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-05-19 17:41:14 +0400

    made a basic graphql api

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.listen(3000)
+app.use('/graphql', graphqlHttp({
+	schema: buildSchema(`
+		type RootQuery{
+			events: [String!]!
+		}
 
-app.get('/',(req,res,next)=>{
-	res.send("Hello World!")
-})
+		type RootMutation{
+			createEvent(name: String): String
+		}
+
+		schema {
+			query: RootQuery
+			mutation: RootMutation
+		}
+	`),
+	rootValue: {
+		events: () => {
+			return ['cooking', 'sports', 'hackathon']
+		},
+
+		createEvent: (args) => {
+			return args.name
+		}
+	},
+	graphiql: true
+}))
+
+app.listen(3000)
