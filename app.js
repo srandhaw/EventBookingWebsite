54e9d3a3ffc36d365bc5e0cae489e3bfb857f10c
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-05-20 17:12:45 +0400

    mongodb setup complete and model created with mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -61,4 +62,12 @@ app.use('/graphql', graphqlHttp({
 	graphiql: true
 }))
 
-app.listen(3000)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-65xqs.mongodb.net/${process.env.MONGO_PASSWORD}?retryWrites=true`).then(
+	()=>{
+		app.listen(3000)
+	}
+).catch(err=>{
+	console.log(err)
+})
+
+
