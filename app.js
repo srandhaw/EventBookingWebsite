addfd0f833f542a36ff3605b68a03dbce23694bd
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-05-21 03:27:50 +0400

    added users collection to MDB atlas

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const bcrypt = require('bcryptjs')
+
 const mongoose = require('mongoose')
 const Event = require('./models/event')
+const User = require('./models/user')
 
 const app = express()
 
@@ -19,11 +22,22 @@ app.use('/graphql', graphqlHttp({
 			date: String!
 		}
 
+		type User{
+			_id: ID!
+			email: String!
+			password: String
+		}
+
 		input EventInput{
 			title: String!
 			description: String!
 			price: Float!
 			date: String!
+		}
+
+		input UserInput{
+			email: String!
+			password: String!
 		}
 
 		type RootQuery{
@@ -32,6 +46,7 @@ app.use('/graphql', graphqlHttp({
 
 		type RootMutation{
 			createEvent(eventInput: EventInput): Event
+			createUser(userInput: UserInput): User
 		}
 
 		schema {
@@ -66,6 +81,32 @@ app.use('/graphql', graphqlHttp({
 				return {...result._doc}
 			}).catch(err =>{
 				console.log(err)
+				throw err
+			})
+			
+		},
+
+		createUser: (args) => {
+			return User.findOne({email: args.userInput.email}).then(user =>{
+				if(user){
+					throw new Error('User exists already')
+				}
+
+			return bcrypt.hash(args.userInput.password, 12)
+			}).then(hashedPassword => {
+				console.log(hashedPassword)
+				const user = new User({
+					email: args.userInput.email,
+					password: hashedPassword
+				})
+				//console.log(user)
+				return user.save()
+			})
+			.then(result => {
+				console.log(result)
+				return {...result._doc, password: null}
+			})
+			.catch(err =>{
 				throw err
 			})
 			
